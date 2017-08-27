9d7f949ccae804bf9a0c3fcb61c1d24168dcdf5d
IDerr <ibrahim@derraz.fr>
2017-08-27 17:41:22 +0200

    fix

--- dealabs/helpers.py
+++ dealabs/helpers.py
@@ -1,12 +1,12 @@
-from .constants import constants as CONSTANTS
+from .constants import API_DEAL_TYPE, API_DEAL_CATEGORIES
 
 
 
 def concatenate_url_type_categories(url, typ, categories):
     if typ is not None:
-        typ = CONSTANTS.API_DEAL_TYPE.format(str(typ))
+        typ = API_DEAL_TYPE.format(str(typ))
         url = '{0}{1}'.format(url, type)
     if categories is not None:
-        categories = CONSTANTS.API_DEAL_CATEGORIES.format(str(categories))
+        categories = API_DEAL_CATEGORIES.format(str(categories))
         url = '{0}{1}'.format(url, categories)
     return url
