4f175a1752f513b8ea18e409b19b7fd874cdcf65
IDerr <ibrahim@derraz.fr>
2019-06-29 11:42:35 +0200

    api new

--- dealabs/dealabs.py
+++ dealabs/dealabs.py
@@ -8,7 +8,14 @@ class Dealabs:
     def __init__(self):
         self.client_key = "539f008401dbb"
         self.client_secret = "539f008401e9c"
-        self.headers = {"Dealabs-App-Version": "com.dealabs.apps.android:82"}
+        self.headers = {
+            'User-Agent': 'com.dealabs.apps.android ANDROID [v5.18.03] [22 | SM-G930K] [@2.0x]',
+            'Pepper-Include-Counters': 'unread_alerts',
+            'Pepper-Include-Prev-And-Next-Ids': 'true',
+            'Pepper-JSON-Format': 'thread=list,group=ids,type=light,event=light,user=full,badge=user,formatted_text=html,message=with_code',
+            'Pepper-Hardware-Id': '5bce296a65215d0bb3b9751bb77b0a1d',
+            'Host': 'www.dealabs.com',
+        }
         self.oauth = OAuth1(self.client_key, client_secret=self.client_secret)
 
     def request(self, url, method='GET', params={}):
@@ -30,3 +37,12 @@ class Dealabs:
         # TODO: check params
         # ex: query=urlencoded(search)&page=int>1&limit=25
         return self.request(url=API_MERCHANT_SEARCH, params=params)
+    
+    def get_new_deals(self, params={}):
+        new_options = {
+            'order_by' : 'new',
+            'limit':'50'
+        }
+        params = {**new_options, **params}
+        req = self.request(url=API_DEAL_THREAD, params=params)
+        return req
--- search_hot.py
+++ search_hot.py
@@ -3,17 +3,5 @@ import json
 import sys
 dealabs = Dealabs()
 
-deals = dealabs.search_deals(
-    params={
-        "order_by": "hot",
-        "query": "1070"
-    }
-)
-
-for deal in deals["data"]:
-    embed = discord.Embed(title=deal["title_formatted"], description="Desc", color=0x00ff00)
-    embed.add_field(name="Price", value=deal["price"], inline=False)
-    embed.add_field(name="HOT", value=deal["temperature_rating"], inline=False)
-    await client.send_message(message.channel, embed=embed)
-    print(deal)
-    sys.exit(0)
+deals = dealabs.get_new_deals()
+print(deals)
