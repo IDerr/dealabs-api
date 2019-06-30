125ca9ebc0e49ffecd672a056c681954844e8db8
IDerr <ibrahim@derraz.fr>
2019-06-30 09:54:45 +0200

    fix hot api + search

--- dealabs/constants.py
+++ dealabs/constants.py
@@ -1,9 +1,6 @@
 from urllib.parse import urljoin
 
 API_BASE_URL = 'https://www.dealabs.com/rest_api/v2/'
-API_DEAL_HOTTEST = urljoin(API_BASE_URL, 'thread/hottest')
 API_DEAL_SEARCH = urljoin(API_BASE_URL, 'thread/search')
 API_DEAL_THREAD = urljoin(API_BASE_URL, 'thread')
-API_MERCHANT_SEARCH = urljoin(API_BASE_URL, 'merchant/search')
-API_MERCHANT_SEARCH = urljoin(API_BASE_URL, 'merchant/search')
 
--- dealabs/dealabs.py
+++ dealabs/dealabs.py
@@ -22,22 +22,26 @@ class Dealabs:
         r = requests.request(method=method, url=url, params=params, headers=self.headers, auth=self.oauth).json()
         return r
 
-    def get_deals_hot(self, params={}):
+    def get_hot_deals(self, params={}):
         # TODO: check params
         # ex: ?days=1&page=0&limit=25
-        return self.request(url=API_DEAL_HOTTEST, params=params)
+        new_options = {
+            'order_by' : 'hot',
+            'limit':'50'
+        }
+        params = {**new_options, **params}
+        return self.request(url=API_DEAL_THREAD, params=params)
 
     def search_deals(self, params):
         # TODO: check params
         # ex: ?order_by=new&type_id=&query=&page=&group_id=&merchant_id=&limit=25&expired=&local=&clearance=
         # order_by = "new", "hot", "discussed", "featured", "new"
+        new_options = {
+            'order_by' : 'hot',
+            'limit':'50'
+        }
         return self.request(url=API_DEAL_SEARCH, params=params)
-    
-    def search_merchant(self, params):
-        # TODO: check params
-        # ex: query=urlencoded(search)&page=int>1&limit=25
-        return self.request(url=API_MERCHANT_SEARCH, params=params)
-    
+      
     def get_new_deals(self, params={}):
         new_options = {
             'order_by' : 'new',
